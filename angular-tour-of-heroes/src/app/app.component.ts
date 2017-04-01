dfa713b8bd5babb6d4a583f04ad6de3daa105ca1
Greg Doud <gpdoud@gmail.com>
2017-04-01 08:45:22 -0400

    angular-tour-of-heroes: finished master/detail

--- angular-tour-of-heroes/src/app/app.component.ts
+++ angular-tour-of-heroes/src/app/app.component.ts
@@ -18,21 +18,24 @@ const HEROES: Hero[] = [
 	{ id: 20, name: 'Tornado' }
 ];
 
+
 @Component({
   selector: 'app-root',
   template: `
   	<h1>{{title}}</h1>
   	<h2>My Heroes</h2>
   	<ul class="heroes">
-  		<li *ngFor='let hero of heroes'>
+  		<li *ngFor='let hero of heroes' (click)='onSelect(hero)' [class.selected]='hero === selectedHero'>
   			<span class='badge'>{{hero.id}}</span> {{hero.name}}
   		</li>
-  	</ul>
-  	<h2>{{hero.name}} details!</h2>
-  	<div><label>id: </label>{{hero.id}}</div>
-  	<div>
-  		<label>name: </label>
-  		<input [(ngModel)]='hero.name' placeholder='name'>
+ 	</ul>
+ 	<div *ngIf='selectedHero'>  	
+ 		<h2>{{selectedHero.name}} details!</h2>
+	  	<div><label>id: </label>{{selectedHero.id}}</div>
+	  	<div>
+	  		<label>name: </label>
+	  		<input [(ngModel)]='selectedHero.name' placeholder='name'>
+	  	</div>
   	</div>
   `,
   //templateUrl: './app.component.html',
@@ -90,9 +93,10 @@ const HEROES: Hero[] = [
 
 export class AppComponent {
   title = 'Tour of Heroes in Angular2';
-  hero: Hero = {
-  	id: 1,
-  	name: 'Windstorm'
-  };
+  selectedHero: Hero;
   heroes = HEROES;
+
+  onSelect(hero: Hero): void {
+	this.selectedHero = hero;	
+  }
 }
